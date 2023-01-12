707f9fb5f275d159e6870e563a1aaf1f25d79717
piro <piro.haniwa@gmail.com>
2023-01-12 18:51:15 +0900

    fix: set space on indent style
    format all files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,11 @@
 const path = require('path')
 
 const buildEslintCommand = (filenames) =>
-	`next lint --fix --file ${filenames
-		.map((f) => path.relative(process.cwd(), f))
-		.join(' --file ')}`
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
 
 module.exports = {
-	'*.{js,jsx,ts,tsx}': [buildEslintCommand],
-	'*.{css,scss,ts,tsx,json,md}': 'prettier --write',
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{css,scss,ts,tsx,json,md}': 'prettier --write',
 }
