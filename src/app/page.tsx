fe4e0dbb5416241707d667db6b729e9a1e606a9a
yanachuwan9sm <gurem919@icloud.com>
2023-03-01 19:25:23 +0900

    build: :building_construction: migration from pages to app directory
    close #64

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+export default async function Home() {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>Welcome to Next.js</h1>
+      </main>
+    </div>
+  )
+}
