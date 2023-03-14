a4431c804227b55325400cbdf53622df7ea8cf9d
Kouhei Yanagi <95360878+yanachuwan9sm@users.noreply.github.com>
2023-03-15 00:48:31 +0900

    feat: :sparkles: implement a blog post listing page (#69)
    * fix: :adhesive_bandage: correction to review comments
    
    * refactor: move css files into the same directory
    
    * refactor: :recycle: make key in list a unique value
    
    * chore: :fire: delete unwanted comments
    
    * refactor: :recycle: fixed the order of the spreadsheet syntax to the proper position
    
    * refactor: :recycle: make the first args of the map() method good
    
    * refactor: :recycle: make code concise by concise body
    
    close #66

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,61 @@
-import styles from '../styles/Home.module.css'
+import * as contentful from 'contentful'
+import { format } from 'date-fns'
+import Link from 'next/link'
+
+import client from 'libs/client'
+
+import styles from './Home.module.css'
+
+const getBlogPosts = async (): Promise<
+  contentful.EntryCollection<Contentful.IBlogPostFields>
+> => {
+  const blogPosts = await client.getEntries<Contentful.IBlogPostFields>({
+    content_type: 'blogPost',
+    order: '-sys.createdAt',
+  })
+  return blogPosts
+}
 
 export default async function Home() {
+  const postDate = await getBlogPosts()
+
+  const postList = postDate.items.map(
+    ({ sys: { id, createdAt, updatedAt }, fields }) => ({
+      ...fields,
+      id,
+      createdAt: format(new Date(createdAt), 'MMM dd, yyyy'),
+      updatedAt: format(new Date(updatedAt), 'MMM dd, yyyy'),
+    })
+  )
+
   return (
     <div className={styles.container}>
+      <header>
+        <div className={styles.title}>
+          <Link href='/'>My Blog</Link>
+        </div>
+      </header>
       <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Next.js</h1>
+        <h1>記事一覧</h1>
+        <ul>
+          {postList.map(({ id, title, category, createdAt }) => (
+            <li key={id}>
+              <article>
+                <h2>
+                  <Link href={`/blog/${id}`}>{title}</Link>
+                </h2>
+                <div>
+                  <div>
+                    {category?.map(({ sys: { id }, fields: { title } }) => (
+                      <div key={id}>{title}</div>
+                    ))}
+                  </div>
+                  <time>{createdAt}</time>
+                </div>
+              </article>
+            </li>
+          ))}
+        </ul>
       </main>
     </div>
   )
