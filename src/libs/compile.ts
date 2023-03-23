df70d99fef05301a8e95c869814c93b0e0ee4678
yanachuwan9sm <gurem919@icloud.com>
2023-03-23 18:12:24 +0900

    feat: :package: add remark-gfm
    close #78

--- src/libs/compile.ts
+++ src/libs/compile.ts
@@ -1,9 +1,13 @@
 import { compileMDX } from 'next-mdx-remote/rsc'
+import remarkGfm from 'remark-gfm'
 
 const compile = async (source: string) => {
   return await compileMDX({
     source,
     options: {
+      mdxOptions: {
+        remarkPlugins: [remarkGfm],
+      },
       parseFrontmatter: false,
     },
   })
