8b4913f69046f7ae4d34f346658464ecbb630929
yanachuwan9sm <gurem919@icloud.com>
2023-04-08 01:51:12 +0900

    feat: :sparkles: extend markdown processing
    close #78

--- src/components/MdxContent/index.tsx
+++ src/components/MdxContent/index.tsx
@@ -1,12 +1,17 @@
 import { MDXRemote } from 'next-mdx-remote/rsc'
 
+import mdxRemoteOption from 'libs/mdx-remote-option'
+
 import type { MDXRemoteProps } from 'next-mdx-remote/rsc'
 
 const MdxContent = (props: MDXRemoteProps) => {
   return (
     // https://github.com/hashicorp/next-mdx-remote/issues/307
     /* @ts-expect-error Async Server Component */
-    <MDXRemote {...props} />
+    <MDXRemote
+      {...props}
+      options={{ ...mdxRemoteOption, ...(props.options || {}) }}
+    />
   )
 }
 
