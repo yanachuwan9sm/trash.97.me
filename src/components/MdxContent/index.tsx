c1bf4fd3620b8e2369d587d2e43c451723798f29
yanachuwan9sm <gurem919@icloud.com>
2023-04-13 23:07:27 +0900

    refactor: :recycle: change the way arguments are received

--- src/components/MdxContent/index.tsx
+++ src/components/MdxContent/index.tsx
@@ -1,20 +1,20 @@
 import { MDXRemote } from 'next-mdx-remote/rsc'
 
-import MdxComponents from 'components/MdxComponents'
+import MdxEmbeddedComponents from 'components/MdxEmbeddedComponents'
 import mdxRemoteOption from 'libs/mdx-remote-option'
 
 import type { MDXRemoteProps } from 'next-mdx-remote/rsc'
 
 type MdxContentProps = MDXRemoteProps
 
-const MdxContent = ({ source, options, components }: MdxContentProps) => {
+const MdxContent = ({ source, ...props }: MdxContentProps) => {
   return (
     // https://github.com/hashicorp/next-mdx-remote/issues/307
     /* @ts-expect-error Async Server Component */
     <MDXRemote
       source={source}
-      options={{ ...mdxRemoteOption, ...(options || {}) }}
-      components={{ ...MdxComponents, ...(components || {}) }}
+      options={{ ...mdxRemoteOption, ...props.options }}
+      components={{ ...MdxEmbeddedComponents, ...props.components }}
     />
   )
 }
