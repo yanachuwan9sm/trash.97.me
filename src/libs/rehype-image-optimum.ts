10add7590af1631dc1f975b9316a676762577232
yanachuwan9sm <gurem919@icloud.com>
2023-04-13 23:09:53 +0900

    refactor: :recycle: reduced overall component code volume

--- src/libs/rehype-image-optimum.ts
+++ src/libs/rehype-image-optimum.ts
@@ -3,41 +3,32 @@ import { Node } from 'unist'
 import { visit } from 'unist-util-visit'
 
 import type { Element } from 'hast'
+import type { Pluggable } from 'unified'
 import type { VFileCompatible } from 'vfile'
 
-const rehypeImageOptimum = () => {
-  return async (tree: Node, _file: VFileCompatible) => {
-    const matches: { node: Element }[] = []
+const rehypeImageOptimum: Pluggable =
+  () => async (tree: Node, _file: VFileCompatible) => {
+    const promiseFuncs: (() => Promise<void>)[] = []
 
-    function visitor(node: Element) {
-      if (
-        node.tagName === 'img' &&
-        node.properties &&
-        node.properties.src &&
-        typeof node.properties.src === 'string'
-      ) {
-        matches.push({ node })
+    visit(tree, 'element', (node: Element) => {
+      if (node.tagName !== 'img' && typeof node?.properties?.src !== 'string') {
+        return
       }
-    }
-
-    visit(tree, 'element', visitor)
-
-    if (!matches.length) return
-
-    const promises = matches.map(async ({ node }) => {
-      if (!node.properties) return
-
-      const src = node.properties.src
-      const blur = await getPlaiceholder(`https:${src}`)
-      node.properties.src = blur.img.src
-      node.properties.width = blur.img.width
-      node.properties.height = blur.img.height
-      node.properties.aspectRatio = `${blur.img.width} / ${blur.img.height}`
-      node.properties.blurDataURL = blur.base64
+      promiseFuncs.push(async () => {
+        if (!node.properties) return
+        const {
+          img: { width, height, src },
+          base64,
+        } = await getPlaiceholder(`https:${node.properties.src}`)
+        node.properties.src = src
+        node.properties.width = width
+        node.properties.height = height
+        node.properties.aspectRatio = `${width} / ${height}`
+        node.properties.blurDataURL = base64
+      })
     })
 
-    await Promise.all(promises)
+    await Promise.all(promiseFuncs.map((func) => func()))
   }
-}
 
 export default rehypeImageOptimum
