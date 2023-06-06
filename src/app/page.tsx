c6725a656fb3699097f65154ece1083b44c3d043
ya7gi <95360878+yanachuwan9sm@users.noreply.github.com>
2023-06-06 17:37:29 +0900

    feat: :sparkles: add Twemoji thumbnail (#111)
    close #110

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import * as contentful from 'contentful'
 import { format } from 'date-fns'
 import Link from 'next/link'
 
+import Twemoji from 'components/Twemoji'
 import client from 'libs/client'
 
 import styles from './Home.module.css'
@@ -20,7 +21,7 @@ export default async function Page() {
   const postDate = await getBlogPosts()
 
   const postList = postDate.items.map(
-    ({ sys: { id, createdAt, updatedAt }, fields }) => ({
+    ({ fields, sys: { createdAt, id, updatedAt } }) => ({
       ...fields,
       id,
       createdAt: format(new Date(createdAt), 'MMM dd, yyyy'),
@@ -38,15 +39,16 @@ export default async function Page() {
       <main className={styles.main}>
         <h1>記事一覧</h1>
         <ul>
-          {postList.map(({ id, title, category, createdAt }) => (
+          {postList.map(({ category, createdAt, id, thumbnail, title }) => (
             <li key={id}>
               <article>
                 <h2>
                   <Link href={`/blog/${id}`}>{title}</Link>
                 </h2>
                 <div>
+                  <Twemoji emoji={thumbnail || ''} height={42} width={42} />
                   <div>
-                    {category?.map(({ sys: { id }, fields: { title } }) => (
+                    {category?.map(({ fields: { title }, sys: { id } }) => (
                       <div key={id}>{title}</div>
                     ))}
                   </div>
